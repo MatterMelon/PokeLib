c8367cb10f1a5d875a9ceb6c3a1e60868a9326c4
Andrey Popov <dil2232@gmail.com>
2025-01-28 19:24:56 +0600

    Change fetchUrl contract, Change other functions

--- src/API/PokemonService.ts
+++ src/API/PokemonService.ts
@@ -1,26 +1,22 @@
-import { pokeApiUrl } from "../globals";
+import { POKEMON_API } from "../globals";
 import { PokeInfoResult, Pokemon } from "../types/PokemonTypes";
 
-async function fetchUrl(url: string) {
+async function fetchUrl<T>(url: string) {
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error(`Response status ${response.status}`);
     }
-    return response.json();
+    return response.json() as T;
 };
 
 export default class PokemonService {
-   static fetchPokemonsInfoPaginated = async (page: number, limit: number) => {
-        const queryUrl =
-          pokeApiUrl +
-          "pokemon/?limit=" +
-          limit +
-          "&offset=" +
-          page * limit;
-        return fetchUrl(queryUrl) as Promise<PokeInfoResult>;
+    static fetchPokemonsInfoPaginated = async (page: number, limit: number) => {
+        const offset = page * limit;
+        const url = POKEMON_API + `pokemon?offset=${offset}&limit=${limit}`;
+        return fetchUrl<PokeInfoResult>(url);
     }
 
     static fetchPokemonByUrl = async (url: string) => {
-        return fetchUrl(url) as Promise<Pokemon>;
+        return fetchUrl<Pokemon>(url);
     };
 }
